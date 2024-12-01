5625ae70334fa640eb395af702d4ecb7c8a0a3e3
JamesBremner <james@ravenspoint.com>
2024-12-01 16:03:56 -0500

    Event queue

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 struct sRequest
@@ -11,7 +12,37 @@ struct sRequest
     int arrival;
 };
 
+struct sEvent
+{
+    enum class eEventType {
+        arrival,
+        completion
+    };
+    eEventType type;
+    sRequest request;
+    int time;
+    sEvent( const sRequest& r )
+    : request( r ), time( r.arrival ),
+    type( eEventType::arrival)
+    {}
+    sEvent( const sRequest& r, int t )
+    : request( r ), time( t ), type( eEventType::completion)
+    {}
+};
+
+class eventComp
+{
+public:
+
+  bool operator() (const sEvent& lhs, const sEvent& rhs) const
+  {
+     return (lhs.time>rhs.time);
+  }
+};
+
 std::vector<sRequest> theRequests;
+
+std::priority_queue<sEvent,std::vector<sEvent>, eventComp> theEventQueue;
 
 void readfile(const std::string &fname)
 {
@@ -32,9 +63,37 @@ void readfile(const std::string &fname)
         ifs >> R.arrival;
     }
 }
+
+void Simulate()
+{
+    // load request arrivals into event queue
+    for( auto & r : theRequests )
+        theEventQueue.push(r);
+    int time = 0;
+    while( theEventQueue.size() )
+    {
+        auto& e = theEventQueue.top();
+        time = e.time;
+        switch( e.type ) {
+            case sEvent::eEventType::arrival:
+            std::cout << e.request.sector << " sector request at " << time << "\n";
+            theEventQueue.push(
+                sEvent( e.request, time + 20 )            );
+            break;
+            case sEvent::eEventType::completion:
+            std::cout << e.request.sector << " sector completed at " << time << "\n";
+            break;
+        }
+        theEventQueue.pop();
+    }
+
+
+}
 main()
 {
     readfile("../dat/test1.txt");
+
+    Simulate();
 
     return 0;
 }
