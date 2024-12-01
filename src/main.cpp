3f4e6a43093c1b8acfd2db6b27c6e841cee6288e
JamesBremner <james@ravenspoint.com>
2024-12-01 15:05:37 -0500

    Implement readfile

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,28 @@ struct sRequest
 
 std::vector<sRequest> theRequests;
 
-void readfile( const std::string& fname )
+void readfile(const std::string &fname)
 {
     // The input will be of the following format with the first number representing the sector to be serviced
     // and the second representing the arrival time (sorted by arrival time asc):
 
+    std::ifstream ifs(fname);
+    if (!ifs.is_open())
+        throw std::runtime_error(
+            "Cannot open " + fname);
+    sRequest R;
+    ifs >> R.sector;
+    ifs >> R.arrival;
+    while (!ifs.fail())
+    {
+        theRequests.push_back(R);
+        ifs >> R.sector;
+        ifs >> R.arrival;
+    }
 }
 main()
 {
-    readfile( "../dat/test1.txt");
+    readfile("../dat/test1.txt");
 
     return 0;
 }
