f545efce40657c81bfa70b1c7bca0938bf60c5ea
JamesBremner <james@ravenspoint.com>
2024-12-01 14:53:38 -0500

    readfile stub

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+void readfile( const std::string& fname )
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
-    {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
 
-        show();
-        run();
-    }
-
-private:
-    wex::label &lb;
-};
-
+}
 main()
 {
-    cGUI theGUI;
+    readfile( "../dat/test1.txt");
+    
     return 0;
 }
