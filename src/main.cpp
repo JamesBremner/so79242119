457bf3e5aca51049e6363670ca104e0277ab7f89
JamesBremner <james@ravenspoint.com>
2024-12-01 14:56:05 -0500

    request data structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,23 @@
 #include <vector>
 #include <algorithm>
 
+struct sRequest
+{
+    int sector;
+    int arrival;
+};
+
+std::vector<sRequest> theRequests;
+
 void readfile( const std::string& fname )
 {
+    // The input will be of the following format with the first number representing the sector to be serviced
+    // and the second representing the arrival time (sorted by arrival time asc):
 
 }
 main()
 {
     readfile( "../dat/test1.txt");
-    
+
     return 0;
 }
